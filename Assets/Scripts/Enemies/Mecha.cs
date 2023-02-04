7079caed3831fc8e9093b5e8defae79af05313b8
Vytautas Gružauskas <manogugas@gmail.com>
2023-02-05 01:08:42 +0200

    started on second enemy

---
+++ Assets/Scripts/Enemies/Mecha.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mecha : Enemy
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}

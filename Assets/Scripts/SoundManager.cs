8c98c284f2bb4685f7308049a61cc9808e5c823e
Vytautas Gružauskas <manogugas@gmail.com>
2023-02-05 11:58:07 +0200

    main sounds and sound distances fixed

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class SoundManager : MonoBehaviour
@@ -10,7 +11,7 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance;
 
     [SerializeField] private AudioSource audioSource;
-    private AudioSource audioSourceOneShot;
+    [SerializeField] private AudioSource audioSourceOneShot;
 
     private void Awake()
     {
@@ -26,12 +27,12 @@ public class SoundManager : MonoBehaviour
 
     }
 
-    public AudioSource PlaySound(AudioClip[] clip, Vector3 position, bool isLooping = false)
+    public AudioSource PlaySound(AudioClip[] clip, Vector3 position, bool isLooping = false, float volume = 1)
     {
-        return PlaySound(clip[Random.Range(0, clip.Length)], position, isLooping);
+        return PlaySound(clip[Random.Range(0, clip.Length)], position, isLooping, volume);
     }
 
-    public AudioSource PlaySound(AudioClip clip, Vector3 position, bool isLooping = false)
+    public AudioSource PlaySound(AudioClip clip, Vector3 position, bool isLooping = false, float volume = 1)
     {
         AudioSource audio = Instantiate(audioSource, position, Quaternion.identity);
         audio.name = "SoundObject";
@@ -39,18 +40,19 @@ public class SoundManager : MonoBehaviour
         audio.clip = clip;
         audio.spatialBlend = 1f;
         audio.dopplerLevel = 0f;
+        audio.volume = volume;
         audio.Play();
         if (!isLooping) Destroy(audio.gameObject, clip.length);
 
         return audio;
     }
 
-    public AudioSource PlaySound(AudioClip[] clip, Transform attachedTransform, bool isLooping = false)
+    public AudioSource PlaySound(AudioClip[] clip, Transform attachedTransform, bool isLooping = false, float volume = 1)
     {
-        return PlaySound(clip[Random.Range(0, clip.Length)], attachedTransform, isLooping);
+        return PlaySound(clip[Random.Range(0, clip.Length)], attachedTransform, isLooping, volume);
     }
 
-    public AudioSource PlaySound(AudioClip clip, Transform attachedTransform, bool isLooping = false)
+    public AudioSource PlaySound(AudioClip clip, Transform attachedTransform, bool isLooping = false, float volume = 1)
     {
         AudioSource audio = Instantiate(audioSource, attachedTransform);
         audio.name = "SoundObject";
@@ -58,6 +60,7 @@ public class SoundManager : MonoBehaviour
         audio.clip = clip;
         audio.spatialBlend = 1f;
         audio.dopplerLevel = 0f;
+        audio.volume = volume;
         audio.Play();
         if (!isLooping) Destroy(audio.gameObject, clip.length);
 
